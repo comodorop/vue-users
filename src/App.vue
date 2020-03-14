64cdd6511eecee9e5504984943ed9c51b0464786
Torres <comodoro49@gmail.com>
2020-03-14 10:31:15 -0600

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,212 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<v-app>
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      
+
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          Users
+        </v-card-title>
+        <v-col>
+          <v-alert v-if="messageShow" type="warning">
+      {{message}}
+    </v-alert>
+          <form>
+          <v-text-field
+          v-model="obj.first_name"
+          :counter="10"
+          label="First name"
+        ></v-text-field>
+        <v-text-field
+          v-model="obj.last_name"
+          :counter="10"
+          label="Last name"
+        ></v-text-field>
+        <v-text-field
+          v-model="obj.email"
+          label="Email"
+        ></v-text-field>
+        <v-text-field
+          v-model="obj.city"
+          :counter="10"
+          label="City"
+          
+        ></v-text-field>
+        
+          </form>
+      
+  </v-col>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            @click="save"
+          >
+            Guardar
+          </v-btn>
+        </v-card-actions>
+        
+      </v-card>
+    </v-dialog>
+  <v-container>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+       <v-row >
+      <v-col cols="4">
+        <v-text-field
+            v-model="search"
+            label="Search ..."
+            outlined
+          ></v-text-field>
+      </v-col>
+      <v-col cols="2">
+        <v-btn x-large @click="getData" color="primary" >Buscar</v-btn>
+      </v-col>
+      <v-col cols="4">
+
+      </v-col>
+      <v-col cols="2">
+        <v-btn x-large @click="openModal" color="success" >Nuevo</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+      <v-simple-table dense >
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th class="text-left">Name</th>
+            <th class="text-left">Last name</th>
+            <th class="text-left">Email</th>
+            <th class="text-left">City</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in desserts" :key="index">
+            <td>{{index +1 }}</td>
+            <td>{{ item.first_name }}</td>
+            <td>{{ item.last_name }}</td>
+            <td>{{ item.email }}</td>
+            <td>{{ item.city }}</td>
+          </tr>
+        </tbody>
+      </template>
+  </v-simple-table>
+      </v-col>
+    </v-row>
+      
+    </v-content>
+    </v-container>
+  </v-app>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import http from './services/http'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+  },
+
+  data: () => ({
+    search: '',
+    desserts: [],
+    dialog: false,
+    obj : {
+      first_name: '',
+      last_name: '',
+      email: '',
+      city: ''
+    },
+    message: '',
+    messageShow: false
+  }),
+  methods: {
+    async getData (){
+      try {
+        let data = await http.get(`v1/users?search=${this.search}`) 
+        this.desserts = data.data.data
+      } catch (error) {
+        console.log(error)
+      }
+      
+    },
+    openModal(){
+      this.dialog = true
+    },
+    async save(){
+      try {
+        let data = await http.post('v1/users', this.obj)  
+        this.dialog = false
+        this.obj.first_name = ''
+        this.obj.last_name = ''
+        this.obj.email  = ''
+        this.obj.city = ''
+      } catch (error) {
+        this.messageShow = true
+        this.message = error.response.data.msg
+        console.log(error.response.data.msg)
+        console.log(error.message)
+        setTimeout(() => {
+          this.messageShow = false
+        }, 1500);
+      }finally {
+        
+      }
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style lang="stylus" scoped>
+
+
 </style>
---
+++ src/services/http.js
@@ -0,0 +1,135 @@
+import axios from "axios";
+
+let baseUrl = "http://localhost:3000/";
+function post(url, data) {
+  var promise = new Promise((resolve, reject) => {
+    axios({
+      method: "post",
+      url: `${baseUrl}${url}`,
+      data: data,
+      headers: {
+        Authorization: localStorage.getItem("token")
+      }
+    })
+      .then(msg => {
+        resolve(msg);
+      })
+      .catch(err => {
+        console.log(err.status);
+        reject(err);
+      });
+  });
+  return promise;
+}
+
+function get(url) {
+  var promise = new Promise((resolve, reject) => {
+    axios({
+      method: "get",
+      url: `${baseUrl}${url}`,
+      headers: {
+        Authorization: localStorage.getItem("token")
+      }
+    })
+      .then(msg => {
+        resolve(msg);
+      })
+      .catch(err => {
+        console.log(err);
+        reject(err);
+      });
+  });
+  return promise;
+}
+
+function deleteData(url, data) {
+  var promise = new Promise((resolve, reject) => {
+    axios({
+      method: "delete",
+      url: `${baseUrl}${url}`,
+      data: data
+    })
+      .then(msg => {
+        resolve(msg);
+      })
+      .catch(err => {
+        console.log(err);
+        reject(err);
+      });
+  });
+  return promise;
+}
+
+function patchData(url, data) {
+  var promise = new Promise((resolve, reject) => {
+    axios({
+      method: "patch",
+      url: `${baseUrl}${url}`,
+      data: data
+    })
+      .then(msg => {
+        resolve(msg);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+  return promise;
+}
+
+function putData(url, data) {
+  var promise = new Promise((resolve, reject) => {
+    axios({
+      method: "put",
+      url: `${baseUrl}${url}`,
+      data: data,
+      headers: {
+        Authorization: localStorage.getItem("token")
+      }
+    })
+      .then(msg => {
+        resolve(msg);
+      })
+      .catch(err => {
+        console.log(err);
+        reject(err);
+      });
+  });
+  return promise;
+}
+
+function refreshToken() {
+  console.log("Va a refrescar el token");
+  var promise = new Promise((resolve, reject) => {
+    axios({
+      method: "post",
+      url: "http://localhost:3000/auth/refresh",
+      data: {
+        grant_type: "refresh_token",
+        refresh_token: localStorage.getItem("refresh_token")
+      }
+    })
+      .then(msg => {
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
+        localStorage.setItem("access_token", msg.data.access_token);
+        localStorage.setItem("refresh_token", msg.data.refresh_token);
+        resolve(1);
+      })
+      .catch(err => {
+        console.log("marco un error");
+        reject(err);
+        console.log(err);
+      });
+  });
+  return promise;
+}
+
+export default {
+  post,
+  get,
+  refreshToken,
+  deleteData,
+  putData,
+  patchData
+};
